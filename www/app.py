830dd2c89ae34894b12cda68a7097676eac1da00
王亚辉 <pc@pcdeiMac.local>
2018-03-14 09:29:47 +0800

    no message

--- www/app.py
+++ www/app.py
@@ -1,4 +1,7 @@
-test
+'''
+async web application.
+'''
+
 import logging; logging.basicConfig(level=logging.INFO)
 
 import asyncio, os, json, time
@@ -9,11 +12,10 @@ from aiohttp import web
 def index(request):
     return web.Response(body=b'<h1>Awesome</h1>')
 
-@asyncio.coroutine
-def init(loop):
+async def init(loop):
     app = web.Application(loop=loop)
     app.router.add_route('GET', '/', index)
-    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
 
