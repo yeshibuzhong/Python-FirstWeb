1d4b0f7e251637e181bfc3e27aac42fa3d330f69
王亚辉 <pc@pcdeiMac.local>
2018-03-22 13:06:39 +0800

    no message

--- www/handlers.py
+++ www/handlers.py
@@ -12,23 +12,36 @@ from coroweb import get, post
 from models import User, Comment, Blog, next_id
 
 @get('/')
-async def index(request):
-    users = await User.findAll()
+def index(request):
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
     return {
-        '__template__': 'test.html',
-        'users': users
+        '__template__': 'blogs.html',
+        'blogs': blogs
     }
 
-@get('/insert')
-async def insert(request):
-    u = User()
-    u.name = 'wanahue';
-    u.email = '123qq.com';
-    u.passwd = 'weglhaisg';
-    u.admin = 'haha';
-    u.image = 'fa.jpg';
-    users = await User.save(u)
-    return {
-        '__template__': 'test.html',
-        'users': users
-    }
+# @get('/')
+# async def index(request):
+#     users = await User.findAll()
+#     return {
+#         '__template__': 'test.html',
+#         'users': users
+#     }
+
+# @get('/insert')
+# async def insert(request):
+#     u = User()
+#     u.name = 'wanahue';
+#     u.email = '123qq.com';
+#     u.passwd = 'weglhaisg';
+#     u.admin = 'haha';
+#     u.image = 'fa.jpg';
+#     users = await User.save(u)
+#     return {
+#         '__template__': 'test.html',
+#         'users': users
+#     }
